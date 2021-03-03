29a4edd9a520f2416815dd368bf74bf1147b04e1
hisarcode <hisarcode@gmail.com>
2021-03-03 17:07:05 +0700

    adding shorcut logo

--- application/views/templates/meta.php
+++ application/views/templates/meta.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets/img/if_new_ico.gif') ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="Infomatika Webinar Series">
 <meta name="author" content="Informatika Untan">
