bf388492febc65178e2666d59f89d2abd30845b6
hisarcode <hisarcode@gmail.com>
2021-03-30 22:00:43 +0700

    move script from js to submenu

--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -149,7 +149,48 @@
     </div>
 
     <?php $this->load->view('templates/script') ?>
+    <script>
+        $(function() {
 
+            $('.tambahSubMenuBtn').on('click', function() {
+                $('#tambahSubMenuModalLabel').html('Tambah Sub Menu Baru');
+                $('.modal-footer button[type=submit]').html('Tambah');
+                $('#title').val('');
+                $('#url').val('');
+                $('#icon').val('');
+                $('#menu_id').val('');
+                $('.modal-body form').attr('action', 'http://localhost/webinarseries/menu/submenu');
+            });
+
+            $('.tampilModalEditSubMenu').on('click', function() {
+
+                $('#tambahSubMenuModalLabel').html('Edit Sub Menu');
+                $('.modal-footer button[type=submit]').html('Edit Data');
+                $('.modal-body form').attr('action', 'http://localhost/webinarseries/menu/editsubmenu');
+
+                const id = $(this).data('id');
+
+                // jquery ajax, request data tanpa mereload seluruh halamannya 
+                $.ajax({
+                    url: 'http://localhost/webinarseries/menu/geteditsubmenu',
+                    data: {
+                        id: id
+                    },
+                    method: 'post',
+                    dataType: 'json',
+                    success: function(data) {
+                        $('#id').val(data.id);
+                        $('#menu_id').val(data.menu_id);
+                        $('#title').val(data.title);
+                        $('#url').val(data.url);
+                        $('#icon').val(data.icon);
+                        $('#is_active').val(data.is_active);
+                    }
+                });
+            });
+
+        });
+    </script>
 </body>
 
 </html>
