657cd3064907578d5313df837826261a37fb461e
hisarcode <hisarcode@gmail.com>
2021-04-08 05:54:04 +0700

    off autocomplete for registrasi

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -21,7 +21,7 @@
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Registrasi Webinar Informatika</h1>
               </div>
-              <form class="user" method="POST" action="<?= base_url('auth/registrasi'); ?>">
+              <form class="user" method="POST" autocomplete="off" action="<?= base_url('auth/registrasi'); ?>">
                 <div class="form-group">
                   <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Lengkap" value="<?= set_value('nama'); ?>">
                   <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
@@ -40,7 +40,7 @@
 
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control" id="passsword1" name="password1" placeholder="Password">
+                    <input autocomplete="new-password" type="password" class="form-control" id="passsword1" name="password1" placeholder="Password">
                     <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                   </div>
                   <div class="col-sm-6">
