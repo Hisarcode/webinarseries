e840ab1bbd2fe539f9ad2a667bbdf00349bd533e
hisarcode <hisarcode@gmail.com>
2021-04-08 05:48:29 +0700

    add link to logout modal in topbar

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -20,20 +20,37 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['nama']; ?></span>
-                <img class="img-profile rounded-circle" src="<?= base_url('assets/img/profile/') . $user['image']; ?>">
+                <?php if (strpos($user['image'], 'https') !== false) : ?>
+                    <img class="img-profile rounded-circle" src="<?= $user['image']; ?>">
+                <?php else : ?>
+                    <img class="img-profile rounded-circle" src="<?= base_url('assets/img/profile/') . $user['image']; ?>">
+                <?php endif; ?>
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    My Profile
-                </a>
 
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="<?= base_url('auth/logout'); ?>">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
-                </a>
+                <?php $role_id = $this->session->userdata('role_id'); ?>
+                <?php if ($role_id != 1) : ?>
+                    <?php if ($role_id == 2) : ?>
+
+                        <a class="dropdown-item" href="<?= base_url('panitia/profil'); ?>">
+                        <?php elseif ($role_id == 3) : ?>
+                            <a class="dropdown-item" href="<?= base_url('peserta/profil'); ?>">
+                            <?php endif; ?>
+
+
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profil Saya
+                            </a>
+                            <div class="dropdown-divider"></div>
+                        <?php endif; ?>
+
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
+
+
             </div>
         </li>
 
