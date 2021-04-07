c1f07ea36ad58463c869fa3e373a4020c4ab9a1e
hisarcode <hisarcode@gmail.com>
2021-04-07 20:23:58 +0700

    create unduh_sertifikat view for peserta

---
+++ application/views/peserta/unduh_sertifikat.php
@@ -0,0 +1,72 @@
+<?php
+class MYPDF extends TCPDF
+{
+
+    //Page header
+    public function Header()
+    {
+        // get the current page break margin
+        $bMargin = $this->getBreakMargin();
+        // get current auto-page-break mode
+        $auto_page_break = $this->AutoPageBreak;
+        // disable auto-page-break
+        $this->SetAutoPageBreak(false, 0);
+        // set bacground image
+        $img_file = APPPATH . $this->CustomHeaderText;
+        $this->Image($img_file, 0, 0, 297, 210, '', '', '', false, 300, '', false, false, 0);
+        // restore auto-page-break status
+        $this->SetAutoPageBreak($auto_page_break, $bMargin);
+        // set the starting point for the page content
+        $this->setPageMark();
+    }
+}
+
+// create new PDF document
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf->CustomHeaderText = "/../upload/sertifikat/" . $sertifikat['gambar_sertifikat'];
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('INFORMATIKA UNTAN');
+$pdf->SetTitle('Sertifikat ' . $sertifikat['webinar_nama']);
+$pdf->SetSubject('Webinar Informatika Untan');
+$pdf->SetKeywords('Untan, Informatika, Teknik, Webinar, Pontianak');
+
+// set header and footer fonts
+$pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(0);
+$pdf->SetFooterMargin(0);
+
+// remove default footer
+$pdf->setPrintFooter(false);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__) . '/lang/eng.php')) {
+    require_once(dirname(__FILE__) . '/lang/eng.php');
+    $pdf->setLanguageArray($l);
+}
+
+// ---------------------------------------------------------
+
+// set font
+$pdf->SetFont('helvetica', '', 26);
+
+// add a page
+$pdf->AddPage('L', 'A4');
+
+$pdf->Ln(60);
+$pdf->Cell(0, 0, $user['nama'], 0, 1, 'C', 0, '', 1);
+
+//Close and output PDF document
+$pdf->Output('Sertifikat ' . $sertifikat['webinar_nama'] . '.pdf', 'I');
