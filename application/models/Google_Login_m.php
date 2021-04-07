7a8083bbefa4071a203e303553f2428ef5d9ed31
hisarcode <hisarcode@gmail.com>
2021-04-08 06:00:08 +0700

    create google_login_m to model for google signin

---
+++ application/models/Google_Login_m.php
@@ -0,0 +1,25 @@
+<?php
+class Google_Login_m extends CI_Model
+{
+    function Is_already_register($id)
+    {
+        $this->db->where('password', $id); // login_oauth_uid
+        $query = $this->db->get('user');
+        if ($query->num_rows() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function Update_user_data($data, $id)
+    {
+        $this->db->where('password', $id);
+        $this->db->update('user', $data);
+    }
+
+    function Insert_user_data($data)
+    {
+        $this->db->insert('user', $data);
+    }
+}
