113393b818706a652c87e99a29f9cab97bd6baa1
hisarcode <hisarcode@gmail.com>
2021-04-07 20:20:11 +0700

    change admin function index title

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -17,7 +17,7 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $data['title'] = "Dashboard";
+        $data['title'] = "Dashboard Admin";
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
 
