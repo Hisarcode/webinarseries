bc10ea48c45c280c8eebc79b4230b3fd545ae514
hisarcode <hisarcode@gmail.com>
2021-04-27 05:20:51 +0700

    add countdown timer on peserta view webinar

--- application/views/peserta/detail_webinar.php
+++ application/views/peserta/detail_webinar.php
@@ -10,6 +10,31 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
     <link rel="stylesheet" type="text/css" href="jquery.timepicker.css" />
     <link rel="stylesheet" type="text/css" href="bootstrap-datepicker.css" />
+    <style>
+        .countdown li {
+            color: black;
+            display: inline-block;
+            font-size: 1rem;
+            list-style-type: none;
+            text-transform: uppercase;
+        }
+
+        .countdown li span {
+            display: block;
+            font-size: 2rem;
+        }
+
+        @media all and (max-width: 768px) {
+            .countdown li {
+                font-size: 1.725rem;
+                padding: 0rem;
+            }
+
+            .countdown li span {
+                font-size: 1.575rem;
+            }
+        }
+    </style>
 
 </head>
 
@@ -73,6 +98,10 @@
                                     </div>
                                 </div>
                                 <div class="col-lg-4">
+                                    <div class=" text-center">
+                                        <h2 class="countdown"></h2>
+                                    </div>
+
                                     <div class="card mb-4">
                                         <div class="card-header">
                                             <h6 class="m-0 font-weight-bold text-primary"> Aksi </h6>
@@ -127,6 +156,81 @@
             <script src="<?= base_url('assets/') ?>/vendor/datatables/jquery.dataTables.min.js"></script>
             <script src="<?= base_url('assets/') ?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
             <script src="<?= base_url('assets/')  ?>/js/demo/datatables-demo.js"></script>
+            <script>
+                const countdown = document.querySelector('.countdown');
+
+
+
+                // set Launch Date (miliSecond)
+
+                const launchDate = new Date('<?= $webinar['tanggal'] ?> <?= $webinar['jam'] ?>:00').getTime();
+
+                console.log(launchDate);
+
+                // Update every second
+
+                const intvl = setInterval(() => {
+
+                    // Get today date and time(miliSecond)
+
+                    const now = new Date().getTime();
+
+
+
+                    // distance from now to the launch date
+
+                    const distance = launchDate - now;
+
+
+
+                    // Time Calculations
+
+                    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+                    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+                    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+                    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+
+                    // Display result
+
+                    countdown.innerHTML = `
+                    <h5 class="text-primary">Acara akan dimulai dalam waktu</h5>
+                    
+                    <li><span>${days}:</span>Hari</li>
+
+                    <li><span>${hours}:</span>Jam</li>
+
+                    <li><span>${mins}:</span>Menit</li>
+
+                    <li><span>${seconds}</span>Detik</li>
+                    
+	`;
+
+
+                    // If launch date passed
+
+                    if (distance < 0) {
+
+                        // Stop CountDown
+
+                        clearInterval(intvl);
+
+                        // Style and output Text
+
+                        countdown.style.color = '#c30000';
+
+                        countdown.innerHTML = 'Webinar telah dilaksanakan';
+
+                    }
+
+
+
+                }, 1000);
+            </script>
         </div>
 
     </div>
