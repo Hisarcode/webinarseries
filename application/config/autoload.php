c9a9a2f406557cf21bf438c1279ba89f0e85b448
hisarcode <hisarcode@gmail.com>
2021-04-27 05:04:55 +0700

    add template to autoload libraries

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('email', 'session', 'database', 'form_validation', 'pdf');
+$autoload['libraries'] = array('email', 'session', 'database', 'form_validation', 'pdf', 'template');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'security', 'pb');
+$autoload['helper'] = array('url', 'file', 'security', 'pb', 'date');
 
 /*
 | -------------------------------------------------------------------
